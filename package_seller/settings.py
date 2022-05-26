a153daff0f6950461166d9d5eebda6cfa25eb32a
admin <admin@Yolobus.local>
2022-05-26 23:03:48 +0530

    all cors enabled

--- package_seller/settings.py
+++ package_seller/settings.py
@@ -55,10 +55,11 @@ INSTALLED_APPS = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
+    'http://*',
+    'https://*',
     'https://package-seller.herokuapp.com',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-    'http://*'
 ]
 
 MIDDLEWARE = [
