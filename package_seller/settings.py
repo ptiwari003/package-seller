b5f8dce5bc86f8d2db406cc621b3ca00c1ba6fb5
admin <admin@Yolobus.local>
2022-05-27 12:56:19 +0530

    Auth cancel

--- package_seller/settings.py
+++ package_seller/settings.py
@@ -56,11 +56,11 @@ INSTALLED_APPS = [
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ]
 }
 CORS_ORIGIN_WHITELIST = [
