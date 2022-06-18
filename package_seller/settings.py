d1e859569278a3c22bf7aaaec2e040df695e0a6d
ptiwari003 <prabhanshu@yolobus.in>
2022-06-18 14:25:49 +0530

    path removed

--- package_seller/settings.py
+++ package_seller/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    'https://package-admin-stage.yolobus.in/',
+    'https://package-admin-stage.yolobus.in',
     'https://package-seller.herokuapp.com',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
